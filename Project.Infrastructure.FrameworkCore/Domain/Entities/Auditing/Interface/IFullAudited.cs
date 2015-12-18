5e4f4b91bb584a6303788ddb2d42c811bca6d54e
awpli <a6818162@163.com>
2015-12-18 19:29:05 +0800

    1111

---
+++ Project.Infrastructure.FrameworkCore/Domain/Entities/Auditing/Interface/IFullAudited.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.Infrastructure.FrameworkCore.Domain.Entities.Auditing.Interface;
+
+namespace Project.Infrastructure.FrameworkCore.Domain.Entities.Auditing
+{
+    public interface IFullAudited:IAudited,IDeletionAudited
+    {
+    }
+}
