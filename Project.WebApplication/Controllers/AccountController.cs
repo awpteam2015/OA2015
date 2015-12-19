3fa8113cf6994107f5faef7cfa48aefaea8b2b7d
awpli <a6818162@163.com>
2015-12-19 12:18:41 +0800

    111

--- Project.WebApplication/Controllers/AccountController.cs
+++ Project.WebApplication/Controllers/AccountController.cs
@@ -16,7 +16,7 @@ namespace Project.WebApplication.Controllers
     public class AccountController : Controller
     {
 
-        [PermissionAuthorize]
+        //[PermissionAuthorize]
         // GET: Account
         public ActionResult Index()
         {
@@ -72,6 +72,12 @@ namespace Project.WebApplication.Controllers
             };
 
         }
+
+        public ActionResult Default()
+        {
+            return View();
+        }
+
 
     }
 
---
+++ Project.WebApplication/Scripts/Account/Index.js
@@ -0,0 +1,34 @@
+﻿
+var pro = pro || {};
+(function () {
+    pro.IndexPage = pro.IndexPage || {};
+    pro.IndexPage = {
+        initPage: function () {
+            $("[name='mainFrame']").attr("src", "/Account/Default");
+
+            $('.menulist li').click(function () {
+                var tabTitle = $(this).find("a").text();
+                var url = $(this).find("a").attr("lang");
+                pro.IndexPage.addTab(tabTitle, url);
+            });
+        },
+        addTab: function (subtitle, url) {
+            if (!$('#tabsDefault').tabs('exists', subtitle)) {
+                $('#tabsDefault').tabs('add', {
+                    title: subtitle,
+                    content: '<iframe scrolling="no" frameborder="0"  src="' + url + '" style="width:100%;height:100%;"></iframe>',
+                    closable: true,
+                    bodyCls: "tabOverflow"
+                });
+            } else {
+                $('#tabsDefault').tabs('select', subtitle);
+            }
+        }
+    };
+})();
+
+
+
+$(function () {
+    pro.IndexPage.initPage();
+});
