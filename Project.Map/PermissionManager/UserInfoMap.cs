ba3d413f7c1e48f5f565686d789f18d1a0e68cd7
awpli <a6818162@163.com>
2015-12-20 19:16:34 +0800

    111

---
+++ Project.Map/PermissionManager/UserInfoMap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.Infrastructure.FrameworkCore.DataNhibernate.EntityMappings;
+using Project.Model.PermissionManager;
+
+namespace Project.Map.PermissionManager
+{
+    public class UserInfoMap : BaseMap<UserInfoEntity, int>
+    {
+        public UserInfoMap(): base("PM_UserInfo")
+        {
+            this.MapPkidDefault<UserInfoEntity,int>();
+
+            Map(p => p.UserCode);
+            Map(p => p.Password);
+            Map(p => p.UserName);
+            Map(p => p.Email);
+            Map(p => p.Mobile);
+            Map(p => p.IsDeleted);
+        }
+    }
+}
