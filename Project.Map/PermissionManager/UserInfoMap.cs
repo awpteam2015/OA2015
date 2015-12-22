27055219aefd561247c7f7a3b201fa6169c6866f
awpli <a6818162@163.com>
2015-12-22 17:22:27 +0800

    1111

--- Project.Map/PermissionManager/UserInfoMap.cs
+++ Project.Map/PermissionManager/UserInfoMap.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using Project.Infrastructure.FrameworkCore.DataNhibernate.EntityMappings;
 using Project.Model.PermissionManager;
 
