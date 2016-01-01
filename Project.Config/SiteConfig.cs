1b82fd6157ed2be4824ccdf755a921aaf370b4f1
awpli <a6818162@163.com>
2016-01-01 09:56:55 +0800

    11

---
+++ Project.Config/SiteConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Config
+{
+    public static class SiteConfig
+    {
+        private static SiteConfigEntiy _siteConfigEntiy;
+
+
+        public static SiteConfigEntiy GetConfig()
+        {
+            return _siteConfigEntiy ?? (_siteConfigEntiy = new SiteConfigEntiy());
+        }
+    }
+}
