42f7acb03c65c5b31555cb5e354ac7570411185b
awpli <a6818162@163.com>
2016-01-05 20:18:07 +0800

    11

---
+++ Project.Map/HRManager/AttendanceMap.cs
@@ -0,0 +1,35 @@
+﻿
+ /***************************************************************************
+ *       功能：     HRAttendance映射类
+ *       作者：     李伟伟
+ *       日期：     2016/1/5
+ *       描述：     
+ * *************************************************************************/
+
+using Project.Infrastructure.FrameworkCore.DataNhibernate.EntityMappings;
+using Project.Model.HRManager;
+using Project.Model.PermissionManager;
+
+namespace  Project.Map.HRManager
+{
+    public class AttendanceMap : BaseMap<AttendanceEntity,int>
+    {
+        public AttendanceMap():base("HR_Attendance")
+        {
+            this.MapPkidDefault<AttendanceEntity,int>();
+ 
+            Map(p => p.EmployeeCode);    
+            Map(p => p.DepartmentCode);    
+            Map(p => p.State);    
+            Map(p => p.Date);    
+            Map(p => p.Remark);    
+            Map(p => p.CreatorUserCode);    
+            Map(p => p.CreatorUserName);    
+            Map(p => p.CreateTime);    
+        }
+    }
+}
+
+    
+ 
+
---
+++ Project.WebApplication/Areas/HRManager/HRManagerAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Project.WebApplication.Areas.HRManager
+{
+    public class HRManagerAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "HRManager";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "HRManager_default",
+                "HRManager/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
