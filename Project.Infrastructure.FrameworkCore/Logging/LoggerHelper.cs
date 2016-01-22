a92bc54c2cf2784e71d409dbc7aa06be102931e3
awpli <a6818162@163.com>
2016-01-22 20:32:13 +0800

    111

--- Project.Infrastructure.FrameworkCore/Logging/LoggerHelper.cs
+++ Project.Infrastructure.FrameworkCore/Logging/LoggerHelper.cs
@@ -12,7 +12,7 @@ namespace Project.Infrastructure.FrameworkCore.Logging
     {
         InfoLogger = 0,
         ErrorLogger = 1,
-        ExceptionLogger = 2,
+        //ExceptionLogger = 2,
         SuccessLogger = 3,
     }
 
@@ -34,12 +34,12 @@ namespace Project.Infrastructure.FrameworkCore.Logging
         {
             if (HttpContext.Current != null)
             {
-                var path = HttpContext.Current.Server.MapPath("~/bin/Config/log4net.config");
+                var path = HttpContext.Current.Server.MapPath("~/Config/log4net.config");
                 var s = XmlConfigurator.Configure(new FileInfo(path));
             }
             else
             {
-                var path = AppDomain.CurrentDomain.BaseDirectory + @"bin\Config\log4net.config";
+                var path = AppDomain.CurrentDomain.BaseDirectory + @"Config\log4net.config";
                 XmlConfigurator.Configure(new FileInfo(path));
             }
         }
--- Project.WebApplication.Tests/logtest.cs
+++ Project.WebApplication.Tests/logtest.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Project.Infrastructure.FrameworkCore.Logging;
 
 namespace Project.WebApplication.Tests
 {
-    class logtest
+      [TestClass]
+  public  class logtest
     {
+          [TestMethod]
+          public void TestMethod1()
+          {
+              LoggerHelper.Error(LogType.InfoLogger,"111");
+          }
     }
 }
