652194fbb669a5f5c1d22704e1b706fc98b38ce1
awpli <a6818162@163.com>
2016-01-22 20:17:37 +0800

    log

---
+++ Project.Infrastructure.FrameworkCore/Logging/CustomLayout/MyPatternLayout.cs
@@ -0,0 +1,12 @@
+﻿using log4net.Layout;
+
+namespace SyncSoft.ROM.Infrastructure.Logging.CustomLayout
+{
+    public class MyPatternLayout : PatternLayout
+    {
+        public MyPatternLayout()
+        {
+            this.AddConverter("property", typeof(MyPatternLayoutConverter));
+        }
+    }
+}
