e78087cd5ca1a4a16f1bb4049bc030c22169bec9
hua7073 <hua7073@163.com>
2016-01-22 21:17:18 +0800

    111

---
+++ Project.WebApplication/Areas/HRManager/Scripts/EmployeeYearDetail/Hd.js
@@ -0,0 +1,118 @@
+﻿var pro = pro || {};
+(function () {
+    pro.EmployeeYearDetail = pro.EmployeeYearDetail || {};
+    pro.EmployeeYearDetail.HdPage = pro.EmployeeYearDetail.HdPage || {};
+    pro.EmployeeYearDetail.HdPage = {
+        initPage: function () {
+            $("#btnAdd").click(function () {
+                pro.EmployeeYearDetail.HdPage.submit("Add");
+            });
+
+            $("#btnEdit").click(function () {
+                pro.EmployeeYearDetail.HdPage.submit("Edit");
+            });
+            
+             $("#btnClose").click(function () {
+                parent.pro.EmployeeYearDetail.ListPage.closeTab("");
+            });
+
+            if ($("#BindEntity").val()) {
+                var bindField = pro.bindKit.getHeadJson();
+                var bindEntity = JSON.parse($("#BindEntity").val());
+                for (var filedname in bindField) {
+                    $("[name=" + filedname + "]").val(bindEntity[filedname]);
+                }
+                //行项目信息用json绑定控件
+                //alert(JSON.stringify(BindEntity.List));
+            }
+        },
+        submit: function (command) {
+            var postData = {};
+            postData.RequestEntity = pro.submitKit.getHeadJson();
+
+            if (pro.commonKit.getUrlParam("PkId") != "") {
+                postData.RequestEntity.PkId = pro.commonKit.getUrlParam("PkId");
+            }
+
+            this.submitExtend.addRule();
+            if (!$("#form1").valid() && this.submitExtend.logicValidate()) {
+                $.alertExtend.error();
+                return false;
+            }
+
+            abp.ajax({
+                url: "/HRManager/EmployeeYearDetail/" + command,
+                data: JSON.stringify(postData)
+            }).done(
+                function (dataresult, data) {
+                   function afterSuccess() {
+                        parent.$("#btnSearch").trigger("click");
+                        parent.pro.EmployeeYearDetail.ListPage.closeTab();
+                    }
+                    parent.$.alertExtend.info("", afterSuccess());
+                }
+            ).fail(
+             function (errordetails, errormessage) {
+               //  $.alertExtend.error();
+             }
+            );
+
+        },
+        submitExtend: {
+            addRule: function () {
+                $("#form1").validate({
+                    rules: {
+          PkId: { required: true  },
+          DepartmentCode: { required: true  },
+          EmployeeCode: { required: true  },
+          BeginDate: { required: true  },
+          EndDate: { required: true  },
+          UseCount: { required: true  },
+          BeforeUseCount: { required: true  },
+          LeftCount: { required: true  },
+          Remark: { required: true  },
+          CreatorUserCode: { required: true  },
+          CreatorUserName: { required: true  },
+          CreateTime: { required: true  },
+          LastModificationTime: { required: true  },
+                    },
+                    messages: {
+          PkId:  "必填!",
+          DepartmentCode:  "部门编号必填!",
+          EmployeeCode:  "员工编号必填!",
+          BeginDate:  "必填!",
+          EndDate:  "必填!",
+          UseCount:  "_decimal合计天数必填!",
+          BeforeUseCount:  "_decimal使用前天数必填!",
+          LeftCount:  "_decimal 年休余数必填!",
+          Remark:  "必填!",
+          CreatorUserCode:  "必填!",
+          CreatorUserName:  "必填!",
+          CreateTime:  "必填!",
+          LastModificationTime:  "必填!",
+                    },
+                    errorPlacement: function (error, element) {
+                        pro.commonKit.errorPlacementHd(error, element);
+                    },
+                    debug: false
+                });
+            },
+            logicValidate: function () {
+                return true;
+            }
+        },
+
+        addTab: function (subtitle, url) {
+
+        }
+
+    };
+})();
+
+
+
+$(function () {
+    pro.EmployeeYearDetail.HdPage.initPage();
+});
+
+
---
+++ Project.WebApplication/Areas/HRManager/Scripts/EmployeeYearDetail/List.js
@@ -0,0 +1,100 @@
+﻿
+var pro = pro || {};
+(function () {
+    pro.EmployeeYearDetail = pro.EmployeeYearDetail || {};
+    pro.EmployeeYearDetail.ListPage = pro.EmployeeYearDetail.ListPage || {};
+    pro.EmployeeYearDetail.ListPage = {
+      init: function () {
+            return {
+                tabObj: new pro.TabBase(),
+                gridObj: new pro.GridBase("#datagrid", false)
+            };
+        },
+        initPage: function () {
+            var initObj = this.init();
+            var tabObj = initObj.tabObj;
+            var gridObj = initObj.gridObj;
+            gridObj.grid({
+                url: '/HRManager/EmployeeYearDetail/GetList',
+                fitColumns: false,
+                nowrap: false,
+                rownumbers: true, //行号
+                singleSelect: true,
+                columns: [[
+         { field: 'PkId', title: '', width: 100 },
+         { field: 'DepartmentCode', title: '部门编号', width: 100 },
+         { field: 'EmployeeCode', title: '员工编号', width: 100 },
+         { field: 'BeginDate', title: '', width: 100 },
+         { field: 'EndDate', title: '', width: 100 },
+         { field: 'UseCount', title: '_decimal合计天数', width: 100 },
+         { field: 'BeforeUseCount', title: '_decimal使用前天数', width: 100 },
+         { field: 'LeftCount', title: '_decimal 年休余数', width: 100 },
+         { field: 'Remark', title: '', width: 100 },
+         { field: 'CreatorUserCode', title: '', width: 100 },
+         { field: 'CreatorUserName', title: '', width: 100 },
+         { field: 'CreateTime', title: '', width: 100 },
+         { field: 'LastModificationTime', title: '', width: 100 },
+                ]],
+                pagination: true,
+                pageSize: 20, //每页显示的记录条数，默认为10     
+                pageList: [20, 30, 40] //可以设置每页记录条数的列表    
+            }
+               );
+
+            $("#btnAdd").click(function () {
+               tabObj.add("/HRManager/EmployeeYearDetail/Hd","新增");
+            });
+
+            $("#btnEdit").click(function () {
+                if (!gridObj.isSelected()) {
+                    $.alertExtend.infoOp();
+                    return;
+                }
+                var PkId = gridObj.getSelectedRow().PkId;
+                tabObj.add("/HRManager/EmployeeYearDetail/Hd?PkId=" + PkId, "编辑" + PkId);
+            });
+
+
+            $("#btnSearch").click(function () {
+                gridObj.search();
+            });
+
+            $("#btnDel").click(function () {
+                if (!gridObj.isSelected()) {
+                $.alertExtend.infoOp();
+                    return;
+                }
+                $.messager.confirm("确认操作", "是否确认删除", function (bl) {
+                    if (!bl) return;
+                    abp.ajax({
+                        url: "/HRManager/EmployeeYearDetail/Delete?PkId=" + gridObj.getSelectedRow().PkId
+                    }).done(
+                    function (dataresult, data) {
+                        $.alertExtend.info();
+                        gridObj.search();
+                    }
+                    ).fail(
+                    function (errordetails, errormessage) {
+                        $.alertExtend.error();
+                    }
+                    );
+                });
+            });
+
+            $("#btnRefresh").click(function () {
+                gridObj.refresh();
+            });
+        },
+         closeTab: function () {
+            this.init().tabObj.closeTab();
+        }
+    };
+})();
+
+
+
+$(function () {
+    pro.EmployeeYearDetail.ListPage.initPage();
+});
+
+
