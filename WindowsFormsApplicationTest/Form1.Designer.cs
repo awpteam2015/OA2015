a246935046b536bafff1ea7637a807a66ded6d5d
Roy <hua7073@163.com>
2016-03-21 12:25:20 +0800

    加入word导出例子

--- WindowsFormsApplicationTest/Form1.Designer.cs
+++ WindowsFormsApplicationTest/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -61,11 +62,22 @@
             this.textBox1.Size = new System.Drawing.Size(126, 162);
             this.textBox1.TabIndex = 2;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(313, 227);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 26);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(537, 330);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -81,6 +93,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- WindowsFormsApplicationTest/Form1.cs
+++ WindowsFormsApplicationTest/Form1.cs
@@ -5,9 +5,11 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Aspose.Words;
 using Project.Infrastructure.FrameworkCore.DataNhibernate;
 using Project.Service.PermissionManager;
 
@@ -25,8 +27,8 @@ namespace WindowsFormsApplicationTest
             var t = UserInfoService.GetInstance().GetModel(1);
 
             var t2 = UserInfoService.GetInstance().GetModel(1);
-           // Thread.CurrentThread.Name = "1111";
-            textBox1.Text += Thread.CurrentThread.Name + "-----------"+t.UserName;
+            // Thread.CurrentThread.Name = "1111";
+            textBox1.Text += Thread.CurrentThread.Name + "-----------" + t.UserName;
 
             SessionFactoryManager.Clear();
         }
@@ -36,5 +38,80 @@ namespace WindowsFormsApplicationTest
             var t = UserInfoService.GetInstance().GetModel(1);
             textBox1.Text += Thread.CurrentThread.Name + "-----------" + t.UserName;
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var dic = new Dictionary<string, string>();
+            dic.Add("姓名", "杨幂");
+            dic.Add("学历", "本科");
+            dic.Add("联系方式", "02759597666");
+            dic.Add("邮箱", "304885433@qq.com");
+            dic.Add("头像", ".//1.jpg");
+            ////使用书签操作
+            //Document doc = new Document(".//1.doc");
+            //DocumentBuilder builder = new DocumentBuilder(doc);
+            //foreach (var key in dic.Keys)
+            //{
+            //    builder.MoveToBookmark(key);
+            //    if (key != "头像")
+            //    {
+            //        builder.Write(dic[key]);
+            //    }
+            //    else
+            //    {
+            //        builder.InsertImage(dic[key]);
+            //    }
+            //}
+            //doc.Save("书签操作.doc");//也可以保存为1.doc 兼容03-07
+            //Console.WriteLine("已经完成书签操作");
+            //使用特殊字符串替换
+            var doc = new Document(".//2.doc");
+            foreach (var key in dic.Keys)
+            {
+                if (key != "头像")
+                {
+                    var repStr = string.Format("&{0}&", key);
+                    doc.Range.Replace(repStr, dic[key], false, false);
+                }
+                else
+                {
+                    Regex reg = new Regex("&头像&");
+                    doc.Range.Replace(reg, new ReplaceAndInsertImage(".//1.jpg"), false);
+                }
+            }
+            doc.Save("字符串替换操作.doc");//也可以保存为1.doc 兼容03-07
+            //Console.WriteLine("已经完成特殊字符串替换操作");
+            //Console.ReadKey();
+        }
+
     }
+
+    public class ReplaceAndInsertImage : IReplacingCallback
+    {
+        /// <summary>
+        /// 需要插入的图片路径
+        /// </summary>
+        public string url { get; set; }
+
+        public ReplaceAndInsertImage(string url)
+        {
+            this.url = url;
+        }
+
+        public ReplaceAction Replacing(ReplacingArgs e)
+        {
+            //获取当前节点
+            var node = e.MatchNode;
+            //获取当前文档
+            Document doc = node.Document as Document;
+            DocumentBuilder builder = new DocumentBuilder(doc);
+            //将光标移动到指定节点
+            builder.MoveTo(node);
+            //插入图片
+            builder.InsertImage(url);
+            return ReplaceAction.Replace;
+        }
+    }
+
+
 }
